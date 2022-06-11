d483f04fd716ad56a9155ec77de59b90f9e09dbe
goranaviani <goranaviani@gmail.com>
2022-06-11 21:40:04 +0200

    react_router_tutorial_part_1

--- src/components/Invoices.js
+++ src/components/Invoices.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {getInvoices} from "../data/Data";
+import {Link} from "react-router-dom";
 
 const Invoices = () => {
+    let invoices = getInvoices()
     return (
-        <div style={{padding: "1rem 0"}}>
-            Invoices
+        <div style={{display: "flex"}}>
+            <nav
+                style={{
+                    borderRight: "solid 1px",
+                    padding: "1rem",
+                }}
+            >
+                {invoices.map((invoice) => (
+                    <Link
+                        style={{display: "block", margin: "1rem 0"}}
+                        to={`/invoices/${invoice.number}`}
+                        key={invoice.number}
+                    >
+                        {invoice.name}
+                    </Link>
+
+                ))}
+
+            </nav>
         </div>
     )
 }
