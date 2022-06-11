2cce255e5ea7942de33657241b30c810c1ed963f
goranaviani <goranaviani@gmail.com>
2022-06-11 13:38:17 +0200

    react_router_tutorial_part_1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {Link} from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-     bookkeeper
-    </div>
-  );
+    return (
+        <div>
+            bookkeeper
+            <nav>
+                <Link to="/invoices">Invoices</Link>
+                <Link to="/expenses">Expenses</Link>
+            </nav>
+        </div>
+    );
 }
 
 export default App;
